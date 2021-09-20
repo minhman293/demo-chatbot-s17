d318e4f54076f5878fe3edccbde6d8bfbdeb1179
minhman293 <mantruong293@gmail.com>
2021-09-20 16:06:57 +0700

    get user name

--- src/services/homeServices.js
+++ src/services/homeServices.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 import request from "request";
 import { response } from "express";
 const la = require("./lang");
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN;
 
 let callSendAPI = (sender_psid, response) => {
     // Construct the message body
@@ -15,7 +16,7 @@ let callSendAPI = (sender_psid, response) => {
     // Send the HTTP request to the Messenger Platform
     request({
         "uri": "https://graph.facebook.com/v11.0/me/messages",
-        "qs": { "access_token": process.env.PAGE_ACCESS_TOKEN },
+        "qs": { "access_token": PAGE_ACCESS_TOKEN },
         "method": "POST",
         "json": request_body
     }, (err, res, body) => {
@@ -25,6 +26,27 @@ let callSendAPI = (sender_psid, response) => {
             console.error("Unable to send message:" + err);
         }
     });
+}
+
+let getUserName = (sender_psid) => {
+    return new Promise((resolve, reject) => {
+        // Send the HTTP request to the Messenger Platform
+        request({
+            "uri": `https://graph.facebook.com/${sender_psid}?fields=first_name,last_name,profile_pic&access_token=${PAGE_ACCESS_TOKEN}`,
+
+            "method": "GET"
+
+        }, (err, res, body) => {
+            if (!err) {
+                body = JSON.parse(body);
+                let username = `${body.last_name} ${body.first_name}`
+                resolve(username);
+            } else {
+                console.error("Unable to send message:" + err);
+                reject(err);
+            }
+        });
+    })
 }
 
 let handleGetStarted = (sender_psid) => {
@@ -70,5 +92,6 @@ let handleIntro = (sender_psid) => {
 
 module.exports = {
     handleGetStarted: handleGetStarted,
-    handleIntro: handleIntro
+    handleIntro: handleIntro,
+    getUserName: getUserName
 }
--- src/services/lang.js
+++ src/services/lang.js
@@ -1,5 +1,8 @@
+import homeServices from "../services/homeServices";
+
+
 module.exports = {
-    START: `Xin chào thế hệ Z của kỷ nguyên số, 
+    START: `Xin chào ${homeServices.getUserName(sender_psid)}, 
 Bạn có thuộc 60% nhóm người gặp bẫy tâm lý về hội chứng bỏ lỡ thông tin hàng ngày không? Mỗi phút trôi qua là hàng triệu tin tức nóng hổi trên mạng xã hội khiến chúng ta dần sinh ra nỗi lo lắng tiềm ẩn có tên là FOMO (Fear Of Missing Out – Tâm lý sợ bỏ lỡ). Vậy không gian nào mới thực sự phù hợp dành cho chúng ta?`,
-    INTRO: 'Với sứ mệnh mang đến giải pháp hữu ích cho cộng đồng, chúng tớ thấu hiểu điều đó và mang đến Boo - chú trợ lý ảo chatbot trên Messenger giúp bạn mang đến trải nghiệm khám phá tin tức về các sự kiện, cuộc thi nhanh nhất dành học sinh - sinh viên'
+    INTRO: 'Với sứ mệnh mang đến giải pháp hữu ích cho cộng đồng, chúng tớ thấu hiểu điều đó và mang đến Boo - chú trợ lý ảo chatbot trên Messenger giúp bạn mang đến trải nghiệm khám phá tin tức về các sự kiện, cuộc thi nhanh nhất dành học sinh - sinh viên.'
 }
